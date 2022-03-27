194f3895ced37c764c68f631868f7e8576ad1a0f
SShak <shawnshaknitz@gmail.com>
2022-03-27 12:00:36 -0400

    Slider for projects

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const sliderItems = [
+    {
+        id: 1,
+        title: "Project 1",
+        desc: "Description",
+    },
+    {
+        id: 2,
+        title: "Project 2",
+        desc: "Description",
+    },
+    {
+        id: 3,
+        title: "Project 3",
+        desc: "Description",
+    },
+    
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
 
 export const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Slider/>
     </div>
   )
 }
