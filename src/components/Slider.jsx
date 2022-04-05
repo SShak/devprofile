43861c87ef63d7f859d07afd836e317bafe5418c
SShak <shawnshaknitz@gmail.com>
2022-04-04 22:10:28 -0400

    clean up

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,4 @@
-import { ArrowLeft, ArrowLeftOutlined, ArrowRight, ArrowRightOutlined } from '@material-ui/icons'
+import { ArrowLeft, ArrowRight } from '@material-ui/icons'
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
