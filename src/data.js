cf0688aba0194faeab8e49f6e7e1e689892509e5
SShak <shawnshaknitz@gmail.com>
2022-04-06 18:57:29 -0400

    additions

--- src/data.js
+++ src/data.js
@@ -4,20 +4,27 @@
 export const sliderItems = [
     {
         id: 1,
+        title: "Dev Portfolio",
+        desc: "This portfolio. The webpage you are looking at now.  Go head, you can check out this code.",
+        link: "https://github.com/SShak/devprofile",
+        img: "publicimg/portfolio.png"
+    },
+    {
+        id: 2,
         title: "Astral Store",
         desc: "This project consists of a frontend design for an ecomerce store. The idea comes from a artist friend that was getting request to buy his work from Instagram messages. The design was based on his overall motif and aesthetic",
         link: "https://github.com/SShak/astralstore",
         img: "publicimg/Astral Store.png"
     },
     {
-        id: 2,
+        id: 3,
         title: "Astral Store Backend",
         desc: "Basically, the backend server for the ecomerce store. Not to flashy, but more necessary.",
         link: "https://github.com/SShak/astralback",
         img: "publicimg/MongoDB Cloud.png"
     },
     {
-        id: 3,
+        id: 4,
         title: "Admin Panel",
         desc: "I wanted to make a modular Admin Panel design that could be converted to many different projects. This is the panel for the shop owner to take care of the many tasks need from one application without ever having to touch the website itself.",
         link: "https://github.com/SShak/admin",
